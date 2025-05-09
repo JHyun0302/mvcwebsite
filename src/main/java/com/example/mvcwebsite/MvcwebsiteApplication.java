3ce7c26942bde766776eab784c18cd9d9f31541c
JHyun <wogustkfkd11@naver.com>
2025-05-09 17:34:43 +0900

    init project

---
+++ src/main/java/com/example/mvcwebsite/MvcwebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.example.mvcwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcwebsiteApplication.class, args);
+	}
+}
